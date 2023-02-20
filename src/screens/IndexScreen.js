74063f8ef2d0cd66d9e6156703f74af02299da1a
Fatih Aydemir <fatihaydemir23@gmail.com>
2023-02-20 12:24:27 +0300

    added: vector icon font packages

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,23 +1,30 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
 import { Context } from '../context/BlogContext';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
+      <Icon name='delete'/>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View> 
+              <Text>{item.title}</Text>
+            </View>
+          )
         }}
       />
     </View>
-    
+
   );
 };
 

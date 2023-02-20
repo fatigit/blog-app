c54337acb2d242311463f443b07b52e898817f99
Fatih Aydemir <fatihaydemir23@gmail.com>
2023-02-20 11:22:57 +0300

    added useReducer instead of useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,3 +1,36 @@
+import React, { useReducer } from "react";
+
+const BlogContext = React.createContext();
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
+    }
+};
+
+export const BlogProvider = ({ children }) => {
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+    const addBlogPost = () => {
+        dispatch({ type: 'add_blogpost' });
+    }
+
+    return (
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+            {children}
+        </BlogContext.Provider>
+    );
+};
+
+export default BlogContext;
+
+
+// Below code includes useState instead of useReducer for same work.
+/*
 import React, { useState } from "react";
 
 const BlogContext = React.createContext();
@@ -19,3 +52,4 @@ export const BlogProvider = ({ children }) => {
 };
 
 export default BlogContext;
+*/
